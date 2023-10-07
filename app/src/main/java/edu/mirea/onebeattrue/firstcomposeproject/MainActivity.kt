8cfe93b9aed845508192785ccf4395415bd46c30
Vitaly <onebeattrue@gmail.com>
2023-10-07 22:00:38 +0300

    Таблица умножения 1) Column - аналог ll vertical, Row - аналог ll horizontal 2) Интерфейс Modifier

--- app/src/main/java/edu/mirea/onebeattrue/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/firstcomposeproject/MainActivity.kt
@@ -1,37 +1,62 @@
 package edu.mirea.onebeattrue.firstcomposeproject
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInfo(name = "Aboba", age = 15)
+            TimesTable()
         }
     }
 }
 
 @Preview
 @Composable
-fun UserInfoPreview() {
-    UserInfo(name = "Aboba", age = 25)
-}
-
-@Composable
-fun UserInfo(name: String, age: Int) {
-    Column {
-        repeat(10) {
-            Text(
-                text = "Name: $name, Age: $age"
-            )
+fun TimesTable() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val backgroundColor = if ((i + j) % 2 == 0) Color.Yellow else Color.White
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(1.dp, Color.Black)
+                            .background(backgroundColor),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(text = "${i * j}")
+                    }
+                }
+            }
         }
     }
 }
